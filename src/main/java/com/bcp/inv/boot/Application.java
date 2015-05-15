634a6f3e80b8401e752798892695a21292d3a8a8
Basil Paulose <bpaulose@tracfone.com>
2015-05-15 13:14:21 +0530

    Modified with new changes

--- src/main/java/com/bcp/inv/boot/Application.java
+++ src/main/java/com/bcp/inv/boot/Application.java
@@ -3,19 +3,22 @@ package com.bcp.inv.boot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer{
+@EnableAutoConfiguration
+@ComponentScan({"com.bcp.inv.controller"})
+public class Application {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
     	
         logger.info("Starting Server!");
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
    
     }
 	
